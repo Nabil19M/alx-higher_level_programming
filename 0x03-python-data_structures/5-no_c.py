d5290861c4913661301b44d5cdb05426777a1b9d
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-03-19 17:18:57 +0200

    peeep

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for s in my_string:
-        if (s != 'c' and s !='C'):
+        if (s != 'c' and s != 'C'):
             new_string += s
     return new_string
