8e06f0817359cf5957389c352a8b384ceee21065
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-03-22 16:59:52 +0200

    roman acrobat

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
     rom = roman_string
     dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    res =  dict[rom[len(roman_string) - 1]]
+    res = dict[rom[len(roman_string) - 1]]
     for i in reversed(range(len(roman_string) - 1)):
         if dict[rom[i]] < dict[rom[i + 1]]:
             res -= (dict[rom[i]])
