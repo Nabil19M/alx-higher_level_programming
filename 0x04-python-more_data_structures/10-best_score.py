763eaa15f7ba4e7599b8432b03add2129b5b59c8
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-03-22 16:52:36 +0200

    pep alert

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     max = 0
     maxkey = None
-    if a_dictionary != None:
+    if a_dictionary is not None:
         for key, value in a_dictionary.items():
             if a_dictionary[key] >= max:
                 maxkey = key
