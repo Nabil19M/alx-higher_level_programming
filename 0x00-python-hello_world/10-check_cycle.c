06b9eaa5a2043515283c47e51c6cf73f1f27d76e
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-03-29 15:58:21 +0200

    check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: head of the list
+ * Return: return 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	slow = fast = list;
+	while (slow)
+	{
+		slow = list->next;
+		fast = list ->next->next;
+		if (slow == fast)
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * is_palindrome - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head);
+{
+    listint_t* h = *head;
+}
