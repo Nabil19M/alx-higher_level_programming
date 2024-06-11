94daf1deb4620f5a90e5502cb4ec3348aec1c9b0
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-06-11 19:36:44 +0300

    Tasks

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title - tv_show_genres.genre_id
 from tv_shows, tv_show_genres
-ORDER BY tv_shows.titles, tv_show_genres.genre_id ;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ;
