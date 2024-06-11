968e1317417b59bdae449d534404e301c0a50d2d
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-06-12 01:57:14 +0300

    Tasks

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -5,6 +5,6 @@ FROM tv_genres
 WHERE tv_genres.name NOT IN (SELECT tv_genres.name
 FROM tv_show_genres
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_genres.name )
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter")
 ORDER BY tv_genres.name ASC;
