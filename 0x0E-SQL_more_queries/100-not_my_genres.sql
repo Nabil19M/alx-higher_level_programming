c96a541158bee845cb7b2a77aad876824d452be3
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-06-12 01:52:51 +0300

    Tasks

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,8 +1,10 @@
 -- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
 -- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-SELECT DISTINCT tv_genres.name
+SELECT tv_genres.name
 FROM tv_show_genres
-RIGHT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title != "Dexter"
+WHERE tv_genres.name NOT IN (SELECT tv_shows.title, tv_genres.name
+FROM tv_show_genres
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_genres.name )
 ORDER BY tv_genres.name ASC;
