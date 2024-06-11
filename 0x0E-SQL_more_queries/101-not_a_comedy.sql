b53a2a5553895cc18e862948cadaf47c34c161e3
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-06-12 01:58:36 +0300

    Tasks

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,9 @@
 -- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
 SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN (SELECT tv_shows.title
 FROM tv_show_genres
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_genres.name != "Comedy"
+WHERE tv_genres.name = "Comedy")
 ORDER BY tv_shows.title ASC;
