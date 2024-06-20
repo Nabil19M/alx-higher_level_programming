aa66216ee1ac37c17926635b6a6b595c8f52642b
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-06-21 01:10:33 +0300

    EXCEPT PyPy

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,9 +3,8 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
-    except ZeroDivisionError:
+    except (TypeError, ZeroDivisionError):
         result = None
-        return (None)
     finally:
         print("inside result: {}".format(result))
-        return (result)
+    return (result)
