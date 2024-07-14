50e9e5640ef4dbbe7e05738bf7defe376ad8b290
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-07-15 01:01:58 +0300

    nuke

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines A function to read files
+    """
+
+
+def read_file(filename=""):
+    """
+        function to read file passing its name as arg
+    Args:
+        filename (str):file name to be read. Defaults to "".
+    """
+    with open(filename, mode="r", encoding="utf-8") as file:
+        for line in file:
+            print(line, end='')
