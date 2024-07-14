7ad8e40bd7b0ef6b279db5da97dd54731d0f4ff9
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-07-15 00:54:11 +0300

    NEW INK

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -12,7 +12,3 @@ def write_file(filename="", text=""):
     """
     with open(filename, mode="w", encoding="utf-8") as file:
         file.write(text)
-
-
-nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-print(nb_characters)
