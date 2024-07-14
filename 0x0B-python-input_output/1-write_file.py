41598c44b49efa370e05fc465b997741b1a1d0d8
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-07-15 00:50:14 +0300

    gimme the pen

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Defines A function to write into files
+    """
+
+
+def write_file(filename="", text=""):
+    """ function to write into files
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        text (str, optional): _description_. Defaults to "".
+    """
+    with open(filename, mode="w", encoding="utf-8") as file:
+        return file.write(text)
+
+
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)
