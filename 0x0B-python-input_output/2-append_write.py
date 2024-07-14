08735e743d5f6e5f1e29511074fab57adabda351
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-07-15 01:24:15 +0300

    json twin

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" Defines A function to overwrite files
+""" Defines A function to write into files
     """
 
 
 def write_file(filename="", text=""):
-    """ function to overwrite files
+    """ function to write into files
 
     Args:
         filename (str, optional): _description_. Defaults to "".
         text (str, optional): _description_. Defaults to "".
     """
-    with open(filename, mode="w", encoding="utf-8") as file:
+    with open(filename, mode="a", encoding="utf-8") as file:
         return file.write(text)
---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" importing json"""
+import json
+""" Defines function that returns json representation of object
+    """
+
+
+def to_json_string(my_obj):
+    return json.dumps(my_obj)
