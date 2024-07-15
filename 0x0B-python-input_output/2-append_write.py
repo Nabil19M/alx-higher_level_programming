89f143efa3d19efe48854615f640a9ff8150398a
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-07-15 18:57:53 +0300

    to the class NOW

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,8 +7,8 @@ def write_file(filename="", text=""):
     """ function to write into files
 
     Args:
-        filename (str, optional): _description_. Defaults to "".
-        text (str, optional): _description_. Defaults to "".
+        filename (str, empty): file name. Defaults to "".
+        text (str, empty): text to be written. Defaults to "".
     """
     with open(filename, mode="a", encoding="utf-8") as file:
         return file.write(text)
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Defines function to json representation of an object
+    """
+
+
+def class_to_join(obj):
+    """ Returns the dictionary description with simple data structure
+    for JSON serialization of an object.
+
+    Args:
+        obj: The object instance to be serialized.
+
+    Returns:
+        dict: A dictionary containing the object's serializable attributes.
+    """
+    return obj.__dict__
