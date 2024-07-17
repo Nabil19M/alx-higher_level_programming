9be2c879748051518de732ca02b1f5c825496ee7
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-07-17 04:53:57 +0300

    width before height

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,8 +23,8 @@ class Rectangle(Base):
             id (int, optional): The id of the rectangle. Defaults to None.
         """
         super().__init__(id)
-        self.height = height
         self.width = width
+        self.height = height
         self.x = x
         self.y = y
 
@@ -51,7 +51,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, height):
-        if type(height) is not int:
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height <= 0:
             raise ValueError("height must be > 0")
